8708d39e60c65e7231555e400de54023e89f7879
James Larsson <jamie@jamescl.com>
2014-08-07 00:32:10 -0400

    Two New Commands
    Sendere and AdminMSG

--- src/com/jamescl/sendere/Sendere.java
+++ src/com/jamescl/sendere/Sendere.java
@@ -142,6 +142,71 @@ public class Sendere extends JavaPlugin{
         this.log( "Sendere has been disabled.", Level.INFO );
     }
     
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if( cmd.getName().equalsIgnoreCase("sendere") ){
+            if( args[0].equals( "reload" ) || args[0].equals( "rl" ) ){
+                if ( !(sender instanceof Player) ) {
+                    this.log( "Reloading configuration...", Level.INFO );
+                    this.reloadConfig();
+                    this.url = this.getConfig().getString("url" );
+                    this.chat = this.getConfig().getBoolean( "Chat" );
+                    this.join = this.getConfig().getBoolean( "Join" );
+                    this.quit = this.getConfig().getBoolean( "Quit" );
+                    this.kick = this.getConfig().getBoolean( "Kick" );
+                    this.death = this.getConfig().getBoolean( "Death" );
+                    this.weather = this.getConfig().getBoolean( "Weather" );
+                    this.log( "Configuration reloaded.", Level.INFO );
+                } else {
+                    if ( sender.hasPermission("sendere.reload") ) {
+                        sender.sendMessage("[Sendere] Reloading configuration...");
+                        this.reloadConfig();
+                        this.url = this.getConfig().getString("url" );
+                        this.chat = this.getConfig().getBoolean( "Chat" );
+                        this.join = this.getConfig().getBoolean( "Join" );
+                        this.quit = this.getConfig().getBoolean( "Quit" );
+                        this.kick = this.getConfig().getBoolean( "Kick" );
+                        this.death = this.getConfig().getBoolean( "Death" );
+                        this.weather = this.getConfig().getBoolean( "Weather" );
+                        sender.sendMessage("[Sendere] Configuration reloaded.");
+                    }
+                }
+            } else {
+                sender.sendMessage( "[Sendere] Version " + this.Version );
+            }
+        }
+                
+        if( args[0].equals( "adminmsg" ) ){
+            if( args.length < 1 ){
+                try{
+                    if ( !( sender instanceof Player ) ) {
+                        for( Player player: Bukkit.getServer().getOnlinePlayers() ){
+                            player.sendMessage( "[Admin Message: Console] " + args[0] );
+                        }
+                        this.publish( "AdminMessage", args[0], "Console" );
+                    } else {
+                        if ( sender.hasPermission("sendere.adminmsg") ) {
+                            for( Player player: Bukkit.getServer().getOnlinePlayers() ){
+                                player.sendMessage( "[Admin Message: " + sender.getName() + "] " + args[0] );
+                            }
+                            this.publish( "AdminMessage", args[0], sender.getName() );
+                        }
+                    }	
+                } catch ( IOException ex ){
+                    Logger.getLogger( Sendere.class.getName() ).log( Level.SEVERE, null, ex );
+                }
+            } else {
+                sender.sendMessage("Not enough arguments!");
+                return false;
+            }
+                
+            return true;
+        }
+          
+        return false; 
+    
+    }
+    
     public void log( String s, Level l ){
         Sendere.log.log( l, "[Sendere] " + s );
     }
